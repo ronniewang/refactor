db1db26193894be04e86e521a5ed20d8fd2c069b
ronniewang <ronniewang1993@gmail.com>
2021-04-24 07:34:33 +0800

    init

---
+++ src/r/w/refactor/This_isaBadnAme.java
@@ -0,0 +1,16 @@
+package r.w.refactor;
+
+public class This_isaBadnAme {
+
+    public This_isaBadnAme() {
+    }
+
+    public static void main(String[] args) {
+        This_isaBadnAme thisisaBadnAme = new This_isaBadnAme();
+    }
+
+    @Override
+    public String toString() {
+        return "This_isaBadnAme{}";
+    }
+}
